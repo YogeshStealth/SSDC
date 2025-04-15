6adc07d7167d29ef4c42e52b427bef61f2b4162a
Yogesh Nandal <yogidesign1996@gmail.com>
2025-04-15 11:09:47 +0530

    Update dependencies and improve Vite configuration
    - Upgraded React and React DOM to version 19.1.0.
    - Updated TypeScript types for React and React DOM to version 19.1.2.
    - Added autoprefixer and Vite React plugin to package.json.
    - Updated Tailwind CSS to version 4.1.4.
    - Enhanced Vite configuration by adjusting the plugin order and resolving the src path using path module.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
